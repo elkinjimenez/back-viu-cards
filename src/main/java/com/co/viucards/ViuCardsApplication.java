22b459e50e704b767c66150d5147cd2777ad496a
Elkin Omar Jiménez García <elkin.jiga@gmail.com>
2023-07-20 17:07:49 -0500

    Initial commit

---
+++ src/main/java/com/co/viucards/ViuCardsApplication.java
@@ -0,0 +1,13 @@
+package com.co.viucards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViuCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViuCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/viucards/ViuCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.viucards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViuCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
